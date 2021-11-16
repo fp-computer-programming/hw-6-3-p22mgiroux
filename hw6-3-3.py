8daafb8f32b9a59dcb67c1ac17b9020a8a880fb8
p22mgiroux <77790842+p22mgiroux@users.noreply.github.com>
2021-11-15 23:06:53 -0500

    Fix hw6-3-3

--- hw6-3-3.py
+++ hw6-3-3.py
@@ -1,10 +1,8 @@
 # Author: MOG 11/15/21
 
-numbers = input("Please enter a list of numbers seperated by spaces: ")
+numbers = input("Please enter a list of 5 numbers seperated by spaces: ")
 
-numb_list = list(numbers)
-numb_list.sort()
-numb_list[:4] = []
+list(numbers)
+final = int(numbers[0]) + int(numbers[2]) + int(numbers[4]) + int(numbers[6]) + int(numbers[8]) 
 
-final = int(numb_list[1]) + int(numb_list[2]) = int(numb_list[3]) + int(numb_list[4]) = int(numb_list[5])
 print(final)
